9b86b087690b783b51708d6f23815474db8f5230
jeeradate <jeeradate@gmail.com>
2024-09-02 10:20:23 +0700

    quate

---
+++ src/LifeCycle.svelte
@@ -0,0 +1,41 @@
+<script>
+    import {onMount} from 'svelte';
+    let quote = '';
+    let author = '';
+    let error = false;
+    let loading = true;
+    onMount(async () => {
+        try {
+            const url = 'https://zenquotes.io/api/random/';
+            const response = await fetch(url);
+            const [quoteMessage] = await response.json();
+            quote = quoteMessage.q;
+            author = quoteMessage.a;
+
+            loading = false;
+        } catch (e){
+            error = true;
+        }
+        loading = false;
+      
+        
+    });
+    import {onDestroy} from 'svelte';
+    onDestroy(() => {
+        console.log('on destroy');
+    })
+
+</script>
+
+{#if loading}
+<h2>Loading...</h2>
+
+{:else if error}
+<h2>Loading Error </h2>
+
+{:else}
+    <h2>{quote}</h2>
+    <h3>{author}</h3>
+
+{/if}
+
