f224fd0aaa673e67f72b5c68a6af94d07c2fd059
jeeradate <jeeradate@gmail.com>
2024-09-02 10:25:03 +0700

    q

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,7 @@
 <script>
     import LifeCycle from "./LifeCycle.svelte";
-   
 </script>
 
-    <LifeCycle />
+<LifeCycle />
 <p>Refresh this page to show different quote</p>
-    <p><a href="https://github.com/jeeradate/pureSvelte">Source code</a></p>
+<p><a href="https://github.com/jeeradate/pureSvelte">Source code</a></p>
