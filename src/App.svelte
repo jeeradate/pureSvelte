3af57c4f8a4bb58a28455d1f3b1f341c4db3d8a5
jeeradate <jeeradate@gmail.com>
2024-09-03 18:01:55 +0700

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,16 @@
 <script>
-    import LifeCycle from "./LifeCycle.svelte";
+    import LifeCycleEvent from "./LifeCycleEvent.svelte";
+    // import Test from "./Test.svelte";
+    // let show = false;
+
+
 </script>
-
-<LifeCycle />
-
+<!-- <label for="checkbox">คลิน checkbox ด้านล่างนี้เพื่อ load component</label> -->
+<!-- <input type="checkbox" bind:checked={show} id="checkbox"> -->
+<!-- <p>-------------------------------------</p> -->
+<!-- {#if show} -->
+    <LifeCycleEvent />
+  
+    
+    
+<!-- {/if} -->
---
+++ src/LifeCycleEvent.svelte
@@ -0,0 +1,63 @@
+<script>
+    import { onMount, onDestroy, beforeUpdate, afterUpdate } from "svelte";
+    let numbers = [];
+    let unorderListElement;
+    let scrollOn = true;
+    let intervalId;
+    let bLabel = 'Run';
+    
+    function toggle(){
+        if(scrollOn){
+            scrollOn = false;
+            pushInterval();
+            bLabel = 'Run';
+        }
+        else{
+            createInterval();
+            bLabel = 'Stop';
+        }
+    
+    }
+    function createInterval() {
+        intervalId = setInterval(() => {
+            numbers = [...numbers, numbers.length + 1];
+        }, 1000);
+        scrollOn = true;
+    }
+    function pushInterval() {
+        clearInterval(intervalId);
+        scrollOn = false;
+    }
+    onMount(createInterval);
+    onDestroy(() => {
+        pushInterval();
+        console.log("on Destroy");
+    });
+    beforeUpdate(() => { 
+        console.log('before update');
+      
+
+
+    });
+    afterUpdate(() => {
+        console.log("after Update");
+        unorderListElement.scrollTo(0, unorderListElement.scrollHeight);
+    });
+</script>
+
+<ul bind:this={unorderListElement}>
+    {#each numbers as number}
+        <li>{number}</li>
+    {/each}
+</ul>
+
+
+<button on:click={toggle}>{bLabel}</button>
+
+<style>
+    ul {
+        max-height: 150px;
+        border: solid black 1px;
+        overflow: scroll;
+    }
+</style>
